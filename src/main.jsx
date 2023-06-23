292b9e38140e5edd6b4076e21d06afaea6208975
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-23 09:41:55 -0500

    configure cors in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://astronomy.api.herokuapp.com";
